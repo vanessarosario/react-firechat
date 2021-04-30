e11adc659eb4f2418b9c0a0de9775f11c23441dd
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-04-30 16:03:41 -0400

    Setup Sing In with Google

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
+import {
+db,
+googleAuthProvider,
+firebase
+} from "./config/firebase";
+import Button from "./components/Button";
 
+function App(){
+
+    const signInWithGoogle = async () => {
+firebase.auth().useDeviceLanguage();
+
+
+  try {
+    await firebase.auth().signInWithPopup(googleAuthProvider);
+     } catch (e) {
+    console.error(e.message);
+     }
+};
+
+return (
+    <div>
+    <Button onClick={signInWithGoogle}> Sing In With Google</Button>
+    </div>
+);
+}
+
+export default App;
