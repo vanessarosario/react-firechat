ee1b4bae5922543e61a6120e9d76f17886175680
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-04-30 15:09:19 -0400

    Crear signInWithGoogle

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ import {
 } from "./config/firebase";
 
 function App() {
+  const signInWithGoogle = async () => {
+    firebase.auth().useDeviceLanguage();
+
+
+     try {
+       await firebase.auth().signInWithPopup(googleAuthProvider);
+     }catch (e) {
+       console.error(e.message);
+     }
+  };
+
+
+
+
   return (
     <div></div>
   );   
