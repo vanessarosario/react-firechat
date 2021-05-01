d517a12237ac1fdc0839f4d26b6d8a1a0f82a224
Adonys Santos <adonysmundotech@gmail.com>
2021-04-30 21:40:41 -0400

    refactor(app): Formatea el documento App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import {
-db,
-googleAuthProvider,
-firebase
-} from "./config/firebase";
+import { db, googleAuthProvider, firebase } from "./config/firebase";
 import Button from "./components/Button";
 
-function App(){
+function App() {
+  const signInWithGoogle = async () => {
+    firebase.auth().useDeviceLanguage();
 
-    const signInWithGoogle = async () => {
-firebase.auth().useDeviceLanguage();
+    try {
+      await firebase.auth().signInWithPopup(googleAuthProvider);
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
 
-
-  try {
-    await firebase.auth().signInWithPopup(googleAuthProvider);
-     } catch (e) {
-    console.error(e.message);
-     }
-};
-
-return (
+  return (
     <div>
-    <Button onClick={signInWithGoogle}> Sing In With Google</Button>
+      <Button onClick={signInWithGoogle}> Sing In With Google</Button>
     </div>
-);
+  );
 }
 
 export default App;
