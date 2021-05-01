d3a8bae42f28c15e4118a0025e56beef308d5d5d
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-04-30 23:49:32 -0400

    Loading With initializing

--- src/App.js
+++ src/App.js
@@ -15,9 +15,18 @@ function App() {
   };
   const { user, initializing} = useAuthState(firebase.auth());
 
+  const renderLoading = () => {
+    if (initializing) {
+      return <div>
+      <h1>Loading ...</h1>
+      </div>
+    }
+
+  }
+
   return (
     <div>
-    {
+    { renderLoading()}
       user ? (
         <>
           <Button onClick={signOut}> Salir</Button>
