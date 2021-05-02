86dfbe6c7a50aea94b77c0279f2cfb7ce20d3ea0
vanessarosariocemas@gmail.com <vanessarosariocemas@gmail.com>
2021-05-02 17:33:56 -0400

    Testing passed in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement =  await waitFor (() => screen.getByText(/Sign in with Google/i));
   expect(linkElement).toBeInTheDocument();
 });
